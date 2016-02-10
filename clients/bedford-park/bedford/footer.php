0bffc5daef7ea2acbc1822cd94e6f4ad88be30a0
nerdswithcharisma <brian@nerdswithcharisma.com>
2016-02-09 19:24:39 -0600

    First push from cmd

--- clients/bedford-park/bedford/footer.php
+++
@@ -1,9 +0,0 @@
-<!-- all js scripts are loaded in library/bones.php -->
-            </section>
-        <?php echo wp_login_url(); ?> 
-
-        <?php wp_footer(); ?>
-    </body>
-    <!-- minified scripts -->
-    <script src="<?php bloginfo('template_directory'); ?>/static/js/global.min.js"></script>
-</html> <!-- end page. what a ride! -->
--- clients/bedford-park/bedford/static/js/util/nwc-toggleNext.js
+++
@@ -1,8 +0,0 @@
-/* ================================================================ */
-/* Toggle the next element by clicking the previous one
-/* ================================================================ */
-jQuery('body').on('click', '.toggleNext', function(e){
-    e.preventDefault();
-    jQuery(this).next().toggleClass('hide');
-    jQuery(this).find('i').toggleClass('fa-caret-down').toggleClass('fa-caret-up');
-});
